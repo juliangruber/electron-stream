44c59a8fc2a01899005850113d2f924a1b446037
Julian Gruber <julian@juliangruber.com>
2017-03-04 14:16:47 +0100

    cli: add --node-integration

--- bin/bin.js
+++ bin/bin.js
@@ -1,8 +1,15 @@
 #!/usr/bin/env node
 
 var electron = require('..');
+var minimist = require('minimist');
 
-var browser = electron();
+var argv = minimist(process.argv.slice(2), {
+  boolean: 'node-integration',
+  alias: {
+    nodeIntegration: 'node-integration'
+  }
+});
+var browser = electron(argv);
 
 process.stdin.pipe(browser);
 browser.stdout.pipe(process.stdout);
